a6b82f93c931622dee2bb11aa5e751234ebbc4ed
Bojana Tesic <tesic.bojana@yahoo.com>
2020-04-03 11:30:26 +0200

    Updates

---
+++ precache-manifest.aec5669e431f71117c1194743db9afd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d109466ee68df952bc70871d2e74dde",
+    "url": "/zavrsni-projekat-macker/index.html"
+  },
+  {
+    "revision": "1ef0321de1c0bf281f1a",
+    "url": "/zavrsni-projekat-macker/static/css/main.393afa8d.chunk.css"
+  },
+  {
+    "revision": "9362b5df72fa40a521aa",
+    "url": "/zavrsni-projekat-macker/static/js/2.6fce4bf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/zavrsni-projekat-macker/static/js/2.6fce4bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef0321de1c0bf281f1a",
+    "url": "/zavrsni-projekat-macker/static/js/main.1d445fa0.chunk.js"
+  },
+  {
+    "revision": "34c8c1081d454232ac90",
+    "url": "/zavrsni-projekat-macker/static/js/runtime-main.f080bfa0.js"
+  }
+]);
